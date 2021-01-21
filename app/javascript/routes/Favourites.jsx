ac75912390ff7787edd90f52dfff8cf9cf5772bf
WinterCore <hogobbl@gmail.com>
2021-01-21 06:28:53 +0200

    Add title to favourites route

--- app/javascript/routes/Favourites.jsx
+++ app/javascript/routes/Favourites.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import FavouritesGrid from '../components/FavouritesGrid';
 
-const Index = () => {
-    return (
-        <div>
-          <FavouritesGrid />
-        </div>
-    );
-};
+import utilStyle from '../utility.module.css';
+
+const Index = () => (
+  <div>
+    <h1 className={utilStyle.heading}>Your Favourites</h1>
+    <FavouritesGrid />
+  </div>
+);
 
 export default Index;
