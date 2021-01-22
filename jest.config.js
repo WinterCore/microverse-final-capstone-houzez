e179c84ca0d10d84486161b6a6c52f8844350696
WinterCore <hogobbl@gmail.com>
2021-01-22 06:06:28 +0200

    Add style & image files resolvers for jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    verbose: true,
-    testPathIgnorePatterns: ["webpack/test.js"],
+  verbose: true,
+  testPathIgnorePatterns: ["webpack/test.js"],
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "\\.(gif|ttf|eot|svg)$": "<rootDir>/app/javascript/tests/file-mock.js",
+  }
 };
