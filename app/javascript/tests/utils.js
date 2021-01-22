bbfbc1807472adf1c380e0c0e6ade2aecd298bac
WinterCore <hogobbl@gmail.com>
2021-01-22 06:08:40 +0200

    Add SnackbarProvider and accept initialRoute

--- app/javascript/tests/utils.js
+++ app/javascript/tests/utils.js
@@ -4,19 +4,26 @@ import { render as rtlRender } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
 import ReduxThunk from 'redux-thunk';
+import { MemoryRouter } from 'react-router-dom';
+import { SnackbarProvider } from 'notistack';
 
-import createStore, { INITIAL_STATE } from '../store/index';
+import { createStore, INITIAL_STATE } from '../store/index';
 
 const render = (
   elem,
   {
     initialState = INITIAL_STATE,
     store = createStore(initialState),
+    initialRoute = '/',
     ...renderOptions
   } = {},
 ) => {
   const Wrapper = ({ children }) => (
-    <Provider store={store}>{children}</Provider>
+    <MemoryRouter initialEntries={[initialRoute]}>
+      <SnackbarProvider>
+        <Provider store={store}>{children}</Provider>
+      </SnackbarProvider>
+    </MemoryRouter>
   );
   Wrapper.propTypes = { children: PropTypes.node.isRequired };
   return rtlRender(elem, { wrapper: Wrapper, ...renderOptions });
