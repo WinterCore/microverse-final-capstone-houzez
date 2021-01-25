4b6335bc36c69651870bd15f32a148362ff9e004
WinterCore <hogobbl@gmail.com>
2021-01-25 18:04:10 +0200

    Enable css & js minifiers for production builds

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,31 @@
-module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('tailwindcss'),
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3
+const plugins = [
+  require('postcss-import'),
+  require('postcss-flexbugs-fixes'),
+  require('tailwindcss'),
+  require('postcss-preset-env')({
+    autoprefixer: {
+      flexbox: 'no-2009'
+    },
+    stage: 3
+  })
+];
+
+// Only run PurgeCSS in production (you can also add staging here)
+if (process.env.RAILS_ENV === "production") {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/*.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/javascript/**/*.vue',
+        './app/javascript/**/*.jsx',
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
     })
-  ]
+  )
 }
+
+module.exports = {
+  plugins,
+};
