14a63c1a8d342621e4f711ffbc451d5a71cc497e
WinterCore <hogobbl@gmail.com>
2021-01-25 18:31:00 +0200

    Remove postcss-purge for now

--- postcss.config.js
+++ postcss.config.js
@@ -10,22 +10,6 @@ const plugins = [
   })
 ];
 
-// Only run PurgeCSS in production (you can also add staging here)
-if (process.env.RAILS_ENV === "production") {
-  plugins.push(
-    require('@fullhuman/postcss-purgecss')({
-      content: [
-        './app/**/*.html.erb',
-        './app/helpers/**/*.rb',
-        './app/javascript/**/*.js',
-        './app/javascript/**/*.vue',
-        './app/javascript/**/*.jsx',
-      ],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
-  )
-}
-
 module.exports = {
   plugins,
 };
