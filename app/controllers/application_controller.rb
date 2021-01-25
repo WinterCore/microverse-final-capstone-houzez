101ae993c94983ea5bb8fce046db234b88a1b07d
WinterCore <hogobbl@gmail.com>
2021-01-25 02:38:47 +0200

    Add sidenav and customize the after_sign_in url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
     def after_sign_out_path_for(resource_or_scope)
         new_admin_session_path
     end
+
+    def after_sign_in_path_for(resource)
+        admin_dashboard_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     end
 
     authenticate :admin do
-      get 'dashboard' => 'dashboard#index', :as => :user_root
+      get 'dashboard' => 'dashboard#index', :as => :admin_dashboard
     end
   end
 
