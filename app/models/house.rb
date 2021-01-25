ce347912849df0a408639705217922ce43d728e6
WinterCore <hogobbl@gmail.com>
2021-01-25 17:19:36 +0200

    Fetch user stats instead of houses

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,8 +2,6 @@ class House < ApplicationRecord
   belongs_to :house_type
   has_many :favourites
   has_many :favouritors, through: :favourites, source: :user
-  scope :group_by_month, -> { group("date_trunc('month', created_at)") }
-  scope :last_year, -> { where('created_at >= ?', (Time.zone.now - 1.year).beginning_of_year) }
 
   scope :belong_to_type, ->(type) { where(house_type_id: type) unless type.nil? }
 
