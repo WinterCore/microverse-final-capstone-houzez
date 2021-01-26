d72f435e7c434d2126da7a19234c5d054043095b
WinterCore <hogobbl@gmail.com>
2021-01-26 03:50:12 +0200

    Optimize tailwindcss production build size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './app/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/javascript/**/*.vue',
+    './app/javascript/**/*.jsx',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
