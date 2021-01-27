fe6bb619edf142d96afaaec0f50c6f78889db687
WinterCore <hogobbl@gmail.com>
2021-01-27 03:46:53 +0200

    Fix linter errors

--- app/javascript/tests/data.js
+++ app/javascript/tests/data.js
@@ -2,19 +2,25 @@ import faker from 'faker';
 
 let id = 0;
 
-export const houseType = () => ({
-  id: ++id,
-  name: faker.name.findName(),
-});
+export const houseType = () => {
+  id += 1;
+  return {
+    id,
+    name: faker.name.findName(),
+  };
+};
 
-export const houseSnippet = () => ({
-  id: ++id,
-  name: faker.name.findName(),
-  images: Array.from({ length: faker.random.number({ min: 1, max: 5 }) })
-    .map(() => faker.image.imageUrl()),
-  price_per_month: faker.random.number({ min: 10, max: 5000 }),
-  house_type: houseType(),
-});
+export const houseSnippet = () => {
+  id += 1;
+  return {
+    id,
+    name: faker.name.findName(),
+    images: Array.from({ length: faker.random.number({ min: 1, max: 5 }) })
+      .map(() => faker.image.imageUrl()),
+    price_per_month: faker.random.number({ min: 10, max: 5000 }),
+    house_type: houseType(),
+  };
+};
 
 export const house = () => ({
   ...houseSnippet(),
@@ -22,9 +28,12 @@ export const house = () => ({
   favourited: faker.random.boolean(),
 });
 
-export const user = () => ({
-  id: ++id,
-  name: faker.name.findName(),
-  email: faker.internet.email(),
-  picture: faker.image.imageUrl(),
-});
+export const user = () => {
+  id += 1;
+  return {
+    id,
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    picture: faker.image.imageUrl(),
+  };
+};
